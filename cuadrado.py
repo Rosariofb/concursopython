8b399d7083685b62f70a4690ef3fb4bc5b67e901
Rosariofb <luzrosariofb@gmail.com>
2020-07-16 11:31:15 -0400

    Create cuadrado.py

---
+++ cuadrado.py
@@ -0,0 +1,9 @@
+def cuadrado(n):
+    lista2=[]
+    for j in range(n):
+        lista1=[]
+        for i in range(n):
+            lista1.append(n)
+        lista2.append(lista1)   
+    return(lista2)
+
